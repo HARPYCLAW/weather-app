f02409b7bf2cb070e4d06dc78563ffb752607e7e
Harshit <harshitvats3007@gmail.com>
2021-07-17 10:26:04 +0530

    weather app created

---
+++ script.js
@@ -0,0 +1,46 @@
+var loc = document.querySelector('.location');
+var value = document.querySelector('.temp-value');
+var climate = document.querySelector('.climate');
+var icon = document.querySelector('.temp-icon');
+
+window.addEventListener('load', () => {
+    var long;
+    var lat;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((position) => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            var proxy = "https://cors-anywhere.herokuapp.com/";
+            const url = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=fa50ad6bad3d99a33562d82f2c2f47cc
+            `;
+            fetch(url)
+            .then(async response => await response.json())
+            .then(data => {
+                console.log(data);
+                const {name} = data;
+                const {feels_like} = data.main;
+                const {id, main} = data.weather[0];
+
+                loc.textContent = name;
+                value.textContent = Math.round(feels_like-273);
+                climate.textContent = main;
+                if(id < 250)
+                icon.src = './icons/thunderstorm.png';
+                else if(id < 350)
+                icon.src = './icons/umbrella.png';
+                else if(id < 550)
+                icon.src = './icons/rain.png';
+                else if(id < 650)
+                icon.src = './icons/snow.png';
+                else if(id < 800)
+                icon.src = './icons/haze.png';
+                else if(id === 800)
+                icon.src = './icons/sun.png';
+                else if(id > 800)
+                icon.src = './icons/cloud.png';
+            });
+        });
+    
+    }
+});
